360e939fc69c88d768c785d28cd3c1177555ca01
Narelsiak <michalekxddlol@gmail.com>
2025-04-08 22:56:43 +0200

    first experiment

--- meta_project/data/data_loader.py
+++ meta_project/data/data_loader.py
@@ -1,5 +1,6 @@
 from .db_controller import DatabaseController
 import pandas as pd
+import random
 
 class DataLoader:
     def __init__(self):
@@ -17,7 +18,7 @@ class DataLoader:
         if df_images.empty or df_classes.empty or df_labels.empty or df_split.empty:
             print("⚠️ Data missing from one of the tables!")
             return None
-        
+
         df = df_images.merge(df_labels, left_on="id", right_on="image_id")
         df = df.merge(df_classes, left_on="class_id", right_on="id", suffixes=("", "_class"))
         df = df.merge(df_split, on="image_id")
@@ -25,8 +26,27 @@ class DataLoader:
         df.drop(columns=["id_class"], inplace=True, errors="ignore")
 
         return df
+    
+    def get_all_class_ids(self):
+        df__classes = self.fetch_table_as_df("classes")
+        return df__classes["id"].tolist() if not df__classes.empty else []
+    
+    def load_data_for_classes(self, class_ids):
+        df_images = self.fetch_table_as_df("images")
+        df_classes = self.fetch_table_as_df("classes")
+        df_labels = self.fetch_table_as_df("image_class_labels")
+        df_split = self.fetch_table_as_df("train_test_split")
 
-data_loader = DataLoader()
-df_combined = data_loader.load_and_merge_data()
+        if df_images.empty or df_classes.empty or df_labels.empty or df_split.empty:
+            print("⚠️ Data missing from one of the tables!")
+            return None
 
-print(df_combined.head())
+        df_classes = df_classes[df_classes["id"].isin(class_ids)]
+        df_labels = df_labels[df_labels["class_id"].isin(class_ids)]
+
+        df = df_images.merge(df_labels, left_on="id", right_on="image_id")
+        df = df.merge(df_classes, left_on="class_id", right_on="id", suffixes=("", "_class"))
+        df = df.merge(df_split, on="image_id")
+        df.drop(columns=["id_class"], inplace=True, errors="ignore")
+
+        return df
